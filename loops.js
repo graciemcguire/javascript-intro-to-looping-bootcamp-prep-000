9e6856d46a31ba6651481eb20465b6ece38c2c6f
Gracie McGuire <m.graciemcguire@gmail.com>
2018-09-19 18:32:13 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,23 @@
+function forLoop(array){
+  for (i = 0; i < 25; i++){
+    if (i === 1){
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push (`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+function whileLoop(num){
+  while (num > 0){
+    console.log (--num);
+  } if (num === 0){
+    return 'done';
+  }
+}
+function doWhileLoop(array){
+  function incrementVariable() {
+  i = i + 1;
+  } //and then write the do-while syntax's?
+}
